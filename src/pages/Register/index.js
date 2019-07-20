d5f8a07b993c1b4982c33f0a7e55c0d91fa051d8
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 18:58:36 -0300

    Imported form from components

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import { Grid, Box, TextField, FormControl, Button } from '@material-ui/core'
+import { TextField, Button } from '@material-ui/core'
+import Form from '../../components/Form';
 
 class Register extends Component {
 
   render() {
     return (
-      <Grid container justify="center" alignItems="center">
-        <Box>
-          <FormControl>
-            <h1>Registro</h1>
-            <TextField required label="Nome"/>
-            <TextField required label="CPF"/>
-            <TextField required label="Telefone"/>
-            <Button variant="contained" color="primary">Registrar</Button>
-          </FormControl>
-        </Box>
-      </Grid>
+      <Form>
+        <h1>Registro</h1>
+        <TextField required label="Nome"/>
+        <TextField required label="CPF"/>
+        <TextField required label="Telefone"/>
+        <Button variant="contained" color="primary">Registrar</Button>
+      </Form>
     );
   }
 }
