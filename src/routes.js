b4ff74ef382d954ce1b3fa0437104f7845700c46
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 19:01:43 -0300

    Created login route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 import Register from './pages/Register'
+import Login from './pages/Login';
 
 const Router = () => {
   return(
     <BrowserRouter>
       <Route path="/" exact component={Register} />
+      <Route path="/login" exact component={Login} />
     </BrowserRouter>
   )
 }
