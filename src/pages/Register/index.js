d998ea3ab8abca5cf6e4000ec652d798f92a14ae
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 18:13:12 -0300

    Created body for register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Grid, Box, TextField, FormControl, Button } from '@material-ui/core'
 
 class Register extends Component {
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <Grid container justify="center" alignItems="center">
+        <Box>
+          <FormControl>
+            <h1>Registro</h1>
+            <TextField required label="Nome"/>
+            <TextField required label="CPF"/>
+            <TextField required label="Telefone"/>
+            <Button variant="contained" color="primary">Registrar</Button>
+          </FormControl>
+        </Box>
+      </Grid>
     );
   }
 }
