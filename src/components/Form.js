f9a8c70bf2b74d2b828b7aa302e8d02bb88369c3
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 23:29:17 -0300

    Added height 100% to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import { Grid, Box, FormControl } from '@material-ui/core'
 class Form extends Component {
   render() {
     return (
-      <Grid container justify="center" alignItems="center">
+      <Grid container justify="center" alignItems="center" style={{height: '100%'}}>
         <Box>
           <FormControl>
             {this.props.children}
