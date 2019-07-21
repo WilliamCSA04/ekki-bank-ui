da22e8aa802e4bbbc2e00f424b309dcae33a9fe1
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 21:39:53 -0300

    Created styled paper

---
+++ src/components/Paper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper as MaterialPaper} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    width: '100%'
+  },
+}));
+
+export default function PaperSheet(props) {
+  const classes = useStyles();
+
+  return (
+    <MaterialPaper className={classes.root} square={props.square} elevation0>
+      {props.children}
+    </MaterialPaper>
+  );
+}
