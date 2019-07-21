409e66ed59eb894559cbb40b0adf212200fd67e1
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 21:20:30 -0300

    Changed variable that get button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,17 +6,17 @@ const useStyles = makeStyles(theme => ({
   margin: {
     'margin-top': theme.spacing(1),
     'margin-bottom': theme.spacing(1),
-  },
+  }
 }));
 
 
 function Button(props){
-  const classes = useStyles()
+  const { margin } = useStyles()
   return (
     <MaterialButton 
       variant={props.variant} 
       color={props.color} 
-      className={classes.margin}>{props.children}
+      className={margin}>{props.children}
     </MaterialButton>
   );
 }
