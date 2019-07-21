02cf18457952abfc72e3a71c419f2955c402ed97
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 21:05:41 -0300

    Removed not used imports of Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import { Button as MaterialButton } from '@material-ui/core'
-import React, { Component } from 'react';
-import { createMuiTheme, withStyles, makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   margin: {
