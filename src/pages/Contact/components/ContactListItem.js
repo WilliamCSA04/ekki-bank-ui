bce4a50afda7295aacd66f47edb352959325d036
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 22:43:27 -0300

    Removed border from inner table of Contact List Item

--- src/pages/Contact/components/ContactListItem.js
+++ src/pages/Contact/components/ContactListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListItemText, ListItem, Icon } from '@material-ui/core';
+import { ListItemText, ListItem, Icon, Table, TableBody, TableRow, TableCell } from '@material-ui/core';
 import Paper from '../../../components/Paper.js';
 import Inline from '../../../components/Inline.js';
 
@@ -9,12 +9,22 @@ class ContactListItem extends Component {
     return (
       <ListItem>
         <Paper>
-          <ListItemText>Nome: Irelia</ListItemText>
-          <Inline>
-            <Icon>attach_money</Icon>
-            <Icon>edit</Icon>
-            <Icon>delete_outline</Icon>
-          </Inline>
+          <Table dense>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{border: "none"}}>
+                  <ListItemText>Nome: Irelia</ListItemText>
+                    <Inline>
+                      <Icon>edit</Icon>
+                      <Icon>delete_outline</Icon>
+                  </Inline>
+                </TableCell>
+                <TableCell align="right" style={{border: "none"}}>
+                  <Icon>chevron_right</Icon>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>    
         </Paper>
       </ListItem>
     );
