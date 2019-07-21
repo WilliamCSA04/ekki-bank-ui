c5b70cce7ab7b28d8606f631a3ffcfdde5ce3cd4
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-21 03:22:11 -0300

    Customized theme of material

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import Router from './routes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple, red } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: purple[500] }, // Purple and green play nicely together.
+    secondary: { main: '#11cb5f' }, // This is just green.A700 as hex.
+    error: { main: red[500] }
+  },
+});
+
+ReactDOM.render(<ThemeProvider theme={theme}><Router /></ThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
