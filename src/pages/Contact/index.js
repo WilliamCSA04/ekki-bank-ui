d819a74c81a346603204b4ddc5ab26d1331a64fe
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 21:40:41 -0300

    Started contact page

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Paper from '../../components/Paper.js';
+import { Typography } from '../../../node_modules/@material-ui/core';
+
+class Contact extends Component {
+  render() {
+    return (
+      <Paper>
+        <Typography>Paper</Typography>
+      </Paper>
+    );
+  }
+}
+
+export default Contact;
