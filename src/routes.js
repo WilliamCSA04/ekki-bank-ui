95bf1c9f2bde46a2b406e05a892da0a89e898f91
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 21:40:55 -0300

    Created route to contact page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 import Register from './pages/Register'
 import Login from './pages/Login';
+import Contact from './pages/Contact';
 
 const Router = () => {
   return(
     <BrowserRouter>
       <Route path="/" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/contact" exact component={Contact} />
     </BrowserRouter>
   )
 }
