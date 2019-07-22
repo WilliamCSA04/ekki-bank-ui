7ffc697a34da826aa59ce93a2dad201f3adadb54
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-22 04:26:44 -0300

    Added state to register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,8 +3,27 @@ import { TextField, Typography } from '@material-ui/core'
 import Button from '../../components/Button.js'
 import Form from '../../components/Form';
 import { Helmet } from "react-helmet";
+import { api } from '../../services/api'
 
 class Register extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      cpf: "",
+      phone: ""
+    }
+  }
+
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onClick = e => {
+    e.preventDefault();
+    console.log(this.state)
+  }
 
   render() {
     return (
@@ -13,11 +32,11 @@ class Register extends Component {
             <title>Registro - Ekki bank</title>
         </Helmet>
         <Form>
-          <Typography component="h1" variant="h5">Login</Typography>
-          <TextField required label="Nome"/>
-          <TextField required label="CPF"/>
-          <TextField required label="Telefone"/>
-          <Button variant="contained" color="primary">Registrar</Button>
+          <Typography component="h1" variant="h5">Registro</Typography>
+          <TextField required label="Nome" name="name" onChange={this.onChange}/>
+          <TextField required label="CPF" name="cpf" onChange={this.onChange}/>
+          <TextField required label="Telefone" name="phone" onChange={this.onChange}/>
+          <Button variant="contained" color="primary" onClick={this.onClick}>Registrar</Button>
         </Form>
       </Fragment>
     );
