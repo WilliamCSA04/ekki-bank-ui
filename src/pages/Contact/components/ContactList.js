af8afb33afdf220f7a32bdb9d3b589dde8add8a5
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-22 05:47:21 -0300

    Added logic to show a message if there is no contacts

--- src/pages/Contact/components/ContactList.js
+++ src/pages/Contact/components/ContactList.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { List } from '@material-ui/core';
+import { List, Typography } from '@material-ui/core';
 import ContactListItem from './ContactListItem.js';
+import api from '../../../services/api';
 
 class ContactList extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      contacts: []
+    }
+  }
+
+  componentDidMount(){
+    const user = JSON.parse(sessionStorage.getItem('ekki-user'));
+    api.get(`/user/${user.id}/contacts`).then(({ data }) => {
+      this.setState({contacts: data})
+    }).catch(err => {
+      if(err.response.data.message){
+        alert(err.response.data.message)
+      }else{
+        alert("Ocorreu um erro ao tentar buscar seus contaos");
+      }
+    })
+  }
+
+  listContacts = () => {
+    if(this.state.contacts.length == 0){
+      return <Typography style={{textAlign: "center"}}>Nenhum contato encontrado</Typography>
+    }else{
+      return <ContactListItem />
+    }
+  }
+
   render() {
     return (
       <List>
-        <ContactListItem />
+        {this.listContacts()}
       </List>
     );
   }
