57dfd001703e0a45997a80252f476acc6f5421d8
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-22 05:31:56 -0300

    Added logic to login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,9 +3,54 @@ import { TextField, Typography } from '@material-ui/core'
 import Button from '../../components/Button.js'
 import Form from '../../components/Form';
 import { Helmet } from "react-helmet";
+import api from '../../services/api'
 
 class Login extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: {
+        cpf: "",
+      },
+      error: {
+        cpf: false,
+      }
+    }
+  }
+
+  onChange = e => {
+    const { data } = this.state;
+    data[e.target.name] = e.target.value
+    this.setState({data: data})
+  }
+
+  onClick = e => {
+    e.preventDefault();
+    const { error, data } = this.state;
+    const noErrors = !(error.cpf)
+    if(noErrors){
+      api.post("/user/signin", data).then(resp => {
+        sessionStorage.setItem("ekki-user", JSON.stringify(resp.data.user))
+        this.props.history.push('/contacts')
+      }).catch(err => {
+        if(err.response.data.message){
+          alert(err.response.data.message)
+        }else{
+          alert("Ocorreu um erro ao tentar logar, tente novamente");
+        }
+      })
+    }
+  }
+
+  validateCpf = e => {
+    const { error } = this.state
+    error.cpf = e.target.value.length != 11
+    this.setState({error: error})
+  }
+
   render() {
+    const { error } = this.state
     return (
       <Fragment>
         <Helmet>
@@ -13,8 +58,8 @@ class Login extends Component {
         </Helmet>
         <Form>
           <Typography component="h1" variant="h5">Login</Typography>
-          <TextField label="CPF"/>
-          <Button variant="contained" color="primary">Login</Button>
+          <TextField label="CPF" name="cpf" error={error.cpf} onBlur={this.validateCpf} onChange={this.onChange}/>
+          <Button variant="contained" color="primary" onClick={this.onClick}>Login</Button>
         </Form>
       </Fragment>
     );
